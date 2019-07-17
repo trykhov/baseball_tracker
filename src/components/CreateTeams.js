1117826732d65981d9c64ba0bf714c1cfbce2358
Try <khovct@gmail.com>
2019-07-16 19:14:00 -0700

    created the layout for the team creation page

--- src/components/CreateTeams.js
+++ src/components/CreateTeams.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import '../css/create_teams.css';
 
 class CreateTeams extends React.Component {
 
 
   render() {
     return(
-      <div>
-        Create Teams
-      </div>
+      <section id="teams">
+        <h1 id="teams_header"> Make Your Teams! </h1>
+        <div id="team_container">
+          <div className="make_team"> Team 1 </div>
+          <div className="make_team"> Team 2 </div>
+        </div>
+      </section>
     )
   }
 }
