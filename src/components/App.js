eca25995ecb248eaa768a43ae514c998739fc419
Try <khovct@gmail.com>
2019-07-16 18:13:57 -0700

    created the menu page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import app from '../css/app.css';
 import Field from './Field';
+import solo_cup from '../img/solo_cup.png';
+import { Link } from 'react-router-dom';
 
 const App = () => {
   return(
-    
     <div id="app">
-      <Field />
+      <h1 id="app_title">Baseball</h1>
+      <img id="solo_cup" src={solo_cup}/>
+      <Link to="/create_teams">
+        <div id="teams_btn"> Create Teams </div>
+      </Link>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { BrowserRouter, Route } from 'react-router-dom';
+import CreateTeams from './components/CreateTeams';
 
 ReactDOM.render(
-  <App/>, document.querySelector("#root")
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={App} exact/>
+      <Route path="/create_teams" component={CreateTeams} />
+
+    </div>
+  </BrowserRouter>, document.querySelector("#root")
 );
