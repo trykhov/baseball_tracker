4a76d3a60500560a9f915c40f6ad257d74751052
Try <khovct@gmail.com>
2019-07-20 17:35:13 -0700

    Updates

---
+++ precache-manifest.9ba6249903d97dd238e863526c8abc2b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c832ba7794f394f1b3b54074d29e27b3",
+    "url": "/baseball_tracker/index.html"
+  },
+  {
+    "revision": "5c118609c17fd72760e0",
+    "url": "/baseball_tracker/static/css/main.5bc6f021.chunk.css"
+  },
+  {
+    "revision": "e5258e59923f76bd384e",
+    "url": "/baseball_tracker/static/js/2.1328814e.chunk.js"
+  },
+  {
+    "revision": "5c118609c17fd72760e0",
+    "url": "/baseball_tracker/static/js/main.7afa3743.chunk.js"
+  },
+  {
+    "revision": "0b56ef9e216d20f5d4c4",
+    "url": "/baseball_tracker/static/js/runtime~main.4ec73964.js"
+  },
+  {
+    "revision": "a1eab4577c535ce2ec60fdb5b9440bb1",
+    "url": "/baseball_tracker/static/media/bases_loaded.a1eab457.png"
+  },
+  {
+    "revision": "c3658506d2998677b17c83a7fcf85234",
+    "url": "/baseball_tracker/static/media/bunt_cup.c3658506.png"
+  },
+  {
+    "revision": "0b580394a08a5ca9428b2db4f77f229d",
+    "url": "/baseball_tracker/static/media/double.0b580394.png"
+  },
+  {
+    "revision": "0590fa0e892bad60ed986c1172163674",
+    "url": "/baseball_tracker/static/media/empty_bases.0590fa0e.png"
+  },
+  {
+    "revision": "127832947631529975d03812fa12a65d",
+    "url": "/baseball_tracker/static/media/homerun.12783294.png"
+  },
+  {
+    "revision": "f6fa06e0890c5235012cfba84a3c376d",
+    "url": "/baseball_tracker/static/media/on_first.f6fa06e0.png"
+  },
+  {
+    "revision": "070f6a7411a13dd60a85c64bc344b57a",
+    "url": "/baseball_tracker/static/media/on_first_second.070f6a74.png"
+  },
+  {
+    "revision": "e9e24497b08bec8fc4bb9cbe266181c9",
+    "url": "/baseball_tracker/static/media/on_first_third.e9e24497.png"
+  },
+  {
+    "revision": "797478f9666a870be91abf1d6a64690f",
+    "url": "/baseball_tracker/static/media/on_second.797478f9.png"
+  },
+  {
+    "revision": "c9176b0f337917dcf4f44a4dc44efff5",
+    "url": "/baseball_tracker/static/media/on_second_third.c9176b0f.png"
+  },
+  {
+    "revision": "500ebc81c66ac62388afe46d960f5156",
+    "url": "/baseball_tracker/static/media/on_third.500ebc81.png"
+  },
+  {
+    "revision": "b30efc4112914ee8417c646ba7b917fb",
+    "url": "/baseball_tracker/static/media/single.b30efc41.png"
+  },
+  {
+    "revision": "edc838766ef2ef87b422492655f4e926",
+    "url": "/baseball_tracker/static/media/solo_cup.edc83876.png"
+  },
+  {
+    "revision": "f000e4fb889bc409404ec640bcfcb1b6",
+    "url": "/baseball_tracker/static/media/triple.f000e4fb.png"
+  }
+]);
